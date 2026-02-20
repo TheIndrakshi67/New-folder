c136e949eb6e182699220c557dfd9f06fb2c5999
TheIndrakshi67 <CoDiNg1234!>
2026-02-20 20:55:31 +0900

    birthday

---
+++ birthday.py
@@ -0,0 +1,15 @@
+name1 = "Alice"
+birthday1 = "April 1st"
+name2 = "Eve"
+birthday2 = "December 12th"
+name3 = "David"
+birthday3 = "March 4th"
+name4 = "Ann"
+birthday4 = "July 25th"
+name5 = "Tom"
+birthday5 = "October 31st"
+print(name1,(":"), birthday1)
+print(name2,(":"), birthday2)
+print(name3,(":"), birthday3)
+print(name4,(":"), birthday4)
+print(name5,(":"), birthday5)
