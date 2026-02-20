a742a4f242ce8e514c2ed737fb012b57868cddfb
TheIndrakshi67 <CoDiNg1234!>
2026-02-20 21:31:49 +0900

    summertime

---
+++ summertimes.py
@@ -0,0 +1,6 @@
+temp= float(input("Enter the current temperature: "))
+if temp>=20:
+    print ("Light and soft clothes are warm enough for ", temp, "degrees celcius" )
+else:
+    print ("In", temp, (" degrees celcius, it is too cold to wear light and soft clothes"))
+    
