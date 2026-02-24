057a14de977b53fbee305618312027d5223380f0
TheIndrakshi67 <CoDiNg1234!>
2026-02-24 17:56:33 +0900

    logicaloperators1

---
+++ logicaloperators.py
@@ -0,0 +1,8 @@
+age= int(input("Enter age: "))
+check= (input("Do you know how to drive? "))
+
+if age>18 and check=="yes":
+    print("You are eligible for a driving license")
+else:
+    print("You are not eligible for a driving license")
+
