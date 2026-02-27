7b6ec226558f88099a5ab8466d60b6494a02999b
TheIndrakshi67 <CoDiNg1234!>
2026-02-27 18:52:56 +0900

    identity

---
+++ identity.py
@@ -0,0 +1,16 @@
+a=  [ 1,2,3,4,5]
+b=  [ 1,2,3,4,5]
+c=a
+
+print(a is b)
+
+print(c is a)
+print (a is not b)
+print ( a is not c)
+
+h = input("Enter your age: ")
+
+if h is 18:
+    print("You are 18 ")
+else:
+    print("You are not 18")
