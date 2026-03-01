3cf32bac78c0895fbd77a602147f370d3504c2b4
TheIndrakshi67 <CoDiNg1234!>
2026-03-01 13:46:35 +0900

    operator precedents

--- grading.py
+++ grading.py
@@ -1,8 +1,8 @@
-Eng= int(input(" Enter your marks for english "))
-Math= int(input(" Enter your marks for maths "))
-Science= int(input(" Enter your marks for science  "))
-Social= int(input(" Enter your marks for Social studies "))
-lang= int(input(" Enter your marks for language "))
+Eng= int(input(" Enter your marks for english; "))
+Math= int(input(" Enter your marks for maths; "))
+Science= int(input(" Enter your marks for science:  "))
+Social= int(input(" Enter your marks for Social studies: "))
+lang= int(input(" Enter your marks for language: "))
 
 avg= (Eng+Math+Science+Social+lang)//5
 
@@ -13,4 +13,5 @@ elif avg>70:
 elif avg>50:
     print("Grade C")
 else:
-    print("YOu failed)")
+    print("You failed")
+
---
+++ operatorpreceednts.py
@@ -0,0 +1,15 @@
+print((4+3*2**2//4-1))
+
+v = 4
+
+w = 5
+
+x = 8
+
+y = 2
+
+z = 0
+
+z = (v+w) * x / y;
+print("Value of (v+w) * x/ y is ", z)
+print(5>3 and 2<1 or not 4==4) #true and false or false
